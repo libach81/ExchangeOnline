3089728747d4799d4e8d41c125de57990ddd7acf
libach81 <44540705+libach81@users.noreply.github.com>
2019-02-28 14:28:52 +0100

    Create ConnectExchangeOnline.ps1

---
+++ ConnectExchangeOnline.ps1
@@ -0,0 +1,5 @@
+#This file connects your local Powershell shell to Exchange Online and imports the cmdlets used for management
+
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session
